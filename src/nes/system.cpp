afc44a28816720d3ec1f4c538d22e1f02453b96f
mellic03 <mellic03@gmail.com>
2025-09-06 21:47:01 +1000

    trt

--- src/nes/system.cpp
+++ src/nes/system.cpp
@@ -15,18 +15,12 @@ NesEmu::System::System()
     ubyte *apuMMIO = mAPU.data<ubyte>();
 
     mBusCPU.attach(&mCPU);
-
-    // CPU --> CPU wRAM,  mirror 2K banks until to 0x1FFF
-    mBusCPU.mapRange(0x0000, 0x1FFF, 2048-1, cpuRAM);
-
-    // CPU --> PPU mmio registers.
-    mBusCPU.mapRange(0x2000, 0x4000, 8-1, ppuMMIO);
-
-    // CPU --> NES APU and IO registers.
-    mBusCPU.mapPage(0x4000, 32-1, apuMMIO);
+    mBusCPU.mapRange(0x0000, 0x1FFF, 2048-1, cpuRAM);  // CPU --> CPU wRAM,  mirror 2K banks until to 0x1FFF
+    mBusCPU.mapRange(0x2000, 0x3FFF, 0x08-1, ppuMMIO); // CPU --> PPU IO regs.
+    mBusCPU.mapPage(0x4000, 32-1, apuMMIO);            // CPU --> NES APU and IO regs.
 
     mBusPPU.attach(&mPPU);
-    mBusPPU.mapPage(0x2000, 32-1, mAPU.data(), RWX::RW);
+    // mBusPPU.mapPage(0x2000, 32-1, mAPU.data(), RWX::RW);
 }
 
 
@@ -54,51 +48,4 @@ void NesEmu::System::tick( uint64_t dt )
         mBusPPU.tick(dt/4);
     }
 }
-
-// void NesEmu::System::LoadROM( uint8_t *rom )
-// {
-//     INESHeader H = *(INESHeader*)rom;
-//     uint16_t prgsize = 16384 * H.prgRomSize;
-//     uint16_t chrsize = 8192  * H.chrRomSize;
-//     memcpy(cpu_rom, rom, prgsize);
-
-//     uint8_t mapno = (0b11110000 & H.flags6) >> 4
-//                   + (0b11110000 & H.flags7) >> 4;
-
-//     bool iNESFormat  = false;
-//     bool NES20Format = false;
-
-//     if (rom[0]=='N' && rom[1]=='E' && rom[2]=='S' && rom[3]==0x1A)
-//     {
-//         if (iNESFormat && (rom[7]&0x0C)==0x08)
-//             NES20Format = true;
-//         else
-//             iNESFormat = true;
-//     }
-
-//     printf("signature   %s\n", H.signature);
-//     printf("prgRomSize  %u\n", prgsize);
-//     printf("chrRomSize  %u\n", chrsize);
-//     printf("mapno       %u\n", mapno);
-//     printf("flags6      %u\n", H.flags6);
-//     printf("flags7      %u\n", H.flags7);
-//     printf("flags8      %u\n", H.flags8);
-//     printf("flags9      %u\n", H.flags9);
-//     printf("flags10     %u\n", H.flags10);
-//     printf("\n");
-
-//     if (iNESFormat)
-//     {
-//         Mapper0(this);
-//     }
-    
-//     if (NES20Format)
-//     {
-        
-//     }
-
-//     cpu.PC = ((uint16_t)cpu_bus.read(0xFFFD) << 8) | cpu_bus.read(0xFFFC);
-//     printf("Reset vector: 0x%04X\n", cpu.PC);
-// }
-
 
